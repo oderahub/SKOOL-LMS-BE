3d91622ca2d957c9c60b76d20b69338e72288286
CHIDERA OKERE <144819255+oderahub@users.noreply.github.com>
2024-06-12 02:40:28 +0100

    skool-lms BE

---
+++ modals/ApplicationDeleted.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Cancel from "/images/Cancelbad.png";
+import MainButton from '../src/components/MainButton';
+
+
+const ApplicationDeleted: React.FC = () => {
+
+  
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="success-container bg-white rounded-lg shadow-lg p-6 max-w-md">
+        <div className="success-icon flex flex-col items-center justify-center">
+          <img src={Cancel} alt="Success" className="justify-center" />
+        </div>
+        <div className="success-message text-center mt-4">
+          <h2 className="text-xl font-semibold">Application Deleted Successfully!</h2>
+          <div className="mt-5">
+            <div className="flex flex-col gap-3 mx-auto bg-gray-200">
+              {/* Pass the handleApproveApplication function with the application ID */}
+              <MainButton button_text={"Close"}  />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ApplicationDeleted;
+ 
---
+++ modals/Applicationsuccess.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import success from "/images/Successgood.png";
+import MainButton from '../src/components/MainButton';
+
+
+const ApplicationSuccessful: React.FC = () => {
+
+  
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="success-container bg-white rounded-lg shadow-lg p-6 max-w-md">
+        <div className="success-icon flex flex-col items-center justify-center">
+          <img src={success} alt="Success" className="justify-center" />
+        </div>
+        <div className="success-message text-center mt-4">
+          <h2 className="text-xl font-semibold">Application Accepted Successfully!</h2>
+          <div className="mt-5">
+            <div className="flex flex-col gap-3 mx-auto bg-gray-200">
+              {/* Pass the handleApproveApplication function with the application ID */}
+              <MainButton button_text={"Close"}  />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ApplicationSuccessful;
